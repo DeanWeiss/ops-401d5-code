6a74f82e4df59fe7a951753ca517a11761bce043
LabPC <dean.w.weiss@gmail.com>
2022-11-16 16:29:58 -0800

    added more

--- challenge32.py
+++ challenge32.py
@@ -1,13 +1,13 @@
 #!/bin/env python3
 
-# Script: Ops 401d5 Challenge 31
+# Script: Ops 401d5 Challenge 32
 # Author: Dean Weiss
-# Date of Last Revision: 14 November 2022
-# Purpose: File Detection 1 of 3
+# Date of Last Revision: 15 November 2022
+# Purpose: File Detection 2 of 3
 
 # Libraries
 from sys import platform
-import os
+import os, hashlib
 
 
 # Declare Functions
@@ -53,4 +53,41 @@ elif platform == 'win32':
     print("This is a windows maching")
     windowsSearch()
 
-# os.system("ls " + str(file) + " | echo \"$(wc -l) files searched.\"")
+
+# Class Demo
+def hash_file(filename):
+    # This function returns the SHA-256 hash of the file pass into it
+
+    # Make a hash object
+    h = hashlib.sha256()
+
+    # open file for reading in binary mode
+    with open(filename, 'rb') as file:
+
+        # loop till the end of the file
+        block = 0
+        while block != b'':
+            # Read only 1024 bytes a time
+            block = file.read(1024)
+            h.update(block)
+            print(h)
+
+    # return the hex representation of the hash digest
+    return h.hexdigest()
+
+# Substitue the file name as the parameter
+message = hash_file("challenge32test.txt")
+print(message)
+
+# Timestamp Function
+
+# Hash Function
+
+# Has getter function
+# use os.walk to crawl through directories and print to the screen all the file hashes
+
+# Menu Options
+# 1. Check OS, search for file
+# 2. Get File Hashes for all files in a folder
+# 3. Profit  .... 
+
